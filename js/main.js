8a8fa08b013486633dcc33d2b881d490e41c5a07
emmaglorypraise <emmaglorypraise@gmail.com>
2020-10-27 15:57:54 +0100

    added dark mode

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-const toggler = document.getElementById("toggle-icon");
-const body = document.body
-const darkmode = document.getElementById("darkmode");
-const lightmode = document.getElementById("lightmode");
-
-toggler.addEventListener("click", function(){
-    (darkmode.setAttribute("href", "css/style.css"));
-});
-
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
 
 
 
